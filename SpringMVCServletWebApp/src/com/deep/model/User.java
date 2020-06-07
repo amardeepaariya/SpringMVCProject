98b633eb67222e487605b9d7517037fdc6616181
Amardeep Kumar <59433305+amardeepaariya@users.noreply.github.com>
2020-06-08 01:06:12 +0530

    Add files via upload

---
+++ SpringMVCServletWebApp/src/com/deep/model/User.java
@@ -0,0 +1,42 @@
+package com.deep.model;
+
+public class User {
+	private String name;
+	private int age;
+	private String email;
+	private String country;
+	
+	
+	public User(String name, int age, String email, String country) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.country = country;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+}
