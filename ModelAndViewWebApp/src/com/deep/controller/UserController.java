d88794a1f22773f8a45e56c9f45e27210d592dd3
Amardeep Kumar <59433305+amardeepaariya@users.noreply.github.com>
2020-06-07 23:35:26 +0530

    Add files via upload

---
+++ ModelAndViewWebApp/src/com/deep/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.deep.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.deep.model.User;
+import com.deep.service.UserService;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/listUsers",method=RequestMethod.GET)
+	public ModelAndView registerSuccess(ModelAndView modelAndView){
+		modelAndView.setViewName("UserList");
+		List<User> userList = userService.getUserList();
+		modelAndView.addObject("userList",userList);
+		return modelAndView;
+		
+	}
+	
+/*	@RequestMapping(value="/listUsers",method=RequestMethod.GET)
+	public ModelAndView registerSuccess(){
+		ModelAndView modelAndView = new ModelAndView("UserList");
+		List<User> userList = userService.getUserList();
+		modelAndView.addObject("userList",userList);
+		return modelAndView;
+		
+	}*/
+
+}
---
+++ ModelAndViewWebApp/src/com/deep/dao/UserDAO.java
@@ -0,0 +1,22 @@
+package com.deep.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.deep.model.User;
+
+@Repository
+public class UserDAO {
+	private static List<User> list=null;
+		static {
+			list = new ArrayList<User>();
+			list.add(new User("kk", 30, "kk.cs@gmail.com", "India"));
+			list.add(new User("Martin", 40, "martine.cs@gmail.com", "US"));
+			list.add(new User("Amardeep", 20, "ak.cs@gmail.com", "India"));
+		}
+	public List<User> getUserList(){
+		return list;
+	}
+}
