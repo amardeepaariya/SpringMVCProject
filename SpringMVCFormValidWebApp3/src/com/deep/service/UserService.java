fc78d9e9ea96b198d02d9c24cb2c8171efe2e465
Amardeep Kumar <59433305+amardeepaariya@users.noreply.github.com>
2020-06-08 00:54:45 +0530

    Add files via upload

---
+++ SpringMVCFormValidWebApp3/src/com/deep/service/UserService.java
@@ -0,0 +1,26 @@
+package com.deep.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.deep.dao.UserDAO;
+import com.deep.model.User;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	public List<User> getUserList() {
+		return userDAO.getUserList();
+	}
+
+	public void createUser(User user) {
+		// TODO Auto-generated method stub
+		userDAO.addUser(user);
+		
+	}
+}
